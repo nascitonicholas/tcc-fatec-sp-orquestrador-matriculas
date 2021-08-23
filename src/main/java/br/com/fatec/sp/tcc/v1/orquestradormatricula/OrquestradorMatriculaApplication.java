ac249f5af1cbe921defb3e0feceaaafb2b92e7ba
nascito_nicholas <nascito.nicholas@gmail.com>
2021-08-23 14:12:13 -0300

    Inicializando projeto.

---
+++ src/main/java/br/com/fatec/sp/tcc/v1/orquestradormatricula/OrquestradorMatriculaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestradormatricula;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrquestradorMatriculaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrquestradorMatriculaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/sp/tcc/v1/orquestradormatricula/OrquestradorMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sp.tcc.v1.orquestradormatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrquestradorMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
